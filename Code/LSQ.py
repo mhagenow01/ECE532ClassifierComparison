cdd49f50a97a75c4d6fbeb1a3f2227d6743b0647
Mike <mhagenow@wisc.edu>
2020-11-27 09:56:16 -0600

    added least squares

--- Code/LSQ.py
+++ Code/LSQ.py
@@ -15,6 +15,18 @@ from PreProcessData import loadFaults
 def lsq(A,b,lam):
     return np.linalg.inv(A.T @ A + lam*np.eye(np.shape(A)[1])) @ A.T @ b
 
+# Calculate the solution to the regularized least squares solution where each sample
+# also has an associated wieght which are stored in vector, w, which is used
+# to perform weighted least squares by converting W to a diagonal matrix
+# source: https://en.wikipedia.org/wiki/Weighted_least_squares
+def wlsq(A,w,b,lam):
+
+    # create diagonal weighting matrix
+    W = np.diag(w.flatten())
+
+    # calculate regularized lsq using weighting matrix
+    return np.linalg.inv(A.T @ W @ A + lam*np.eye(np.shape(A)[1])) @ A.T @ W @ b
+
 
 def test():
     X_faults = loadFaults()
@@ -40,7 +52,7 @@ def test():
 
 def testdirect():
     X_faults = loadFaults()
-    X_train_plus1 = X_faults[0]
+    X_train_plus1 = X_faults[3]
     X_train_minus1 = X_faults[1]
     X_train = np.vstack((X_train_plus1, X_train_minus1))
     y_train = np.vstack((np.ones((np.shape(X_train_plus1)[0], 1)), -np.ones((np.shape(X_train_minus1)[0], 1))))
@@ -59,8 +71,33 @@ def testdirect():
     error_vec = [0 if i[0] == i[1] else 1 for i in np.hstack((y_hat_test, y_test))]
     print("Lam:", 0.0, " Error:", sum(error_vec) / len(y_test))
 
+
+def testVall():
+    X_faults = loadFaults()
+    num_class = len(X_faults)
+    ii = 3
+
+    # positive label is the 'one'
+    X_train_plus1 = X_faults[ii]
+
+    # negative label is the 'all' randomly downsampled to the same length as the positive
+    X_train_minus1 = np.vstack([X_faults[i] for i in range(0, num_class) if (i != ii)])
+
+    X_train_temp = np.vstack((X_train_plus1, X_train_minus1))
+    y_train_temp = np.vstack((np.ones((np.shape(X_train_plus1)[0], 1)), -np.ones((np.shape(X_train_minus1)[0], 1))))
+    w_train_temp = np.vstack((1.0 / (np.shape(X_train_plus1)[0]) * np.ones((np.shape(X_train_plus1)[0], 1)),
+                              1.0 / (np.shape(X_train_minus1)[0]) * np.ones((np.shape(X_train_minus1)[0], 1))))
+
+    w = wlsq(X_train_temp, w_train_temp, y_train_temp, 0.0)
+    y_hat_test = np.sign(X_train_temp @ w)
+    error_vec = [0 if i[0] == i[1] else 1 for i in np.hstack((y_hat_test, y_train_temp))]
+    print("Lam:", 0.0, " Error:", sum(error_vec) / len(y_train_temp))
+
+    print((X_train_temp @ w)[0:10])
+
+
 if __name__ == "__main__":
-    testdirect()
+    testVall()
 
 
 
