879a29d3d8352f29198af9eab4c38216cb014a35
Mike <mhagenow@wisc.edu>
2020-11-29 08:02:23 -0600

    add backprop shell

--- Code/simpleNN.py
+++ Code/simpleNN.py
@@ -34,13 +34,31 @@ class simpleNN(nn.Module):
 
 
 def nn(A,b,lam):
+    # create an instance of the network
+    net = simpleNN(np.shape(A)[1])
+    loss_fxn = torch.nn.functional.mse_loss()
+    # create your optimizer
+    optimizer = optim.SGD(net.parameters(), lr=0.01)
 
+    for ii in range(0,10):
+
+        rand_ind = np.random.choice(np.shape(A)[0])
+        network_in = A[rand_ind,:]
+        network_target = b[rand_ind]
+
+        # in your training loop:
+        optimizer.zero_grad()  # zero the gradient buffers
+        output = net.forward(network_in)
+        loss = loss_fxn(output, network_target)
+        loss.backward()
+        optimizer.step()  # Does the update
+
+        #print("Epoch:", ii, "Training Loss: ", np.mean(loss.item()), "Valid Loss: ", np.mean(valid_loss))
 
 
 
 
 def test():
-    print("hello")
 
 if __name__ == "__main__":
     test()
