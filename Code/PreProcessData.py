e64e957f440490b56876038b8e89d0b99868afd2
Mike <mhagenow@wisc.edu>
2020-12-01 20:12:22 -0600

    add shell to load mocap data, working on bad characters

--- Code/PreProcessData.py
+++ Code/PreProcessData.py
@@ -55,6 +55,52 @@ def loadFaults():
     print("---")
     return X_faults
 
+def loadMocap():
+    # define that space yo yo
+    # todo: change to package directory
+
+    data = np.loadtxt('./../Data/Postures.csv',delimiter=',',skiprows=2)
+    print(np.shape(data))
+
+    # First 27 attributes are the independent variables
+    X = data[:,0:27]
+
+    # Normalize the columns of X
+    X = X/np.linalg.norm(X,axis=0)
+
+    # Concatenate 1's on the end as an offset feature
+    # X = np.hstack((X,np.ones((np.shape(X)[0],1))))
+
+    print(np.shape(X))
+
+    print(np.linalg.matrix_rank(X))
+
+    # Final 7 attributes are 1/0 for the type of fault
+    Y = data[:,27:]
+
+    # print(np.shape(X))
+    # print(np.shape(Y))
+    #
+    # # Confirms that each plate has only one type of fault
+    # print(np.max(np.sum(Y,axis=1)))
+
+    X_faults = []
+    # seven types of faults for list
+    for ii in range(0,7):
+        X_faults.append(np.zeros((0,27)))
+
+    # Store as the following X_faults is a list of the data for each type of fault
+    # Don't need a Y in this case
+
+    for ii in range(0,np.shape(X)[0]):
+        fault_ind_temp = np.argmax(Y[ii,:])
+        X_faults[fault_ind_temp] = np.vstack((X_faults[fault_ind_temp],X[ii,:]))
+
+    for ii in range(0,7):
+        print(ii,"-",np.shape(X_faults[ii]))
+    print("---")
+    return X_faults
+
 if __name__ == "__main__":
     loadFaults()
 
