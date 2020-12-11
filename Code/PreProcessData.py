9ad80565475b2c12cada02df487ddcfabe6d77fe
Mike <mhagenow@wisc.edu>
2020-12-11 12:08:09 -0600

    work on mocap features

--- Code/PreProcessData.py
+++ Code/PreProcessData.py
@@ -8,6 +8,7 @@ to test with the implemented algorithms
 __author__ = "Mike Hagenow"
 
 import numpy as np
+from itertools import combinations
 
 def loadFaults():
     # define that space yo yo
@@ -61,6 +62,8 @@ def loadMocap():
     # indicator value for missing - use to determine equal feature lengths
     ind_full = [i for i in range(0,np.shape(data)[0]) if data[i,19]!=-999.999]
 
+    print(len(ind_full))
+
     data_full = data[ind_full]
 
     Y = data_full[:,0]
@@ -76,19 +79,43 @@ def loadMocap():
 
         # Using 6 markers
         num_markers = 6
-        x_vals_temp = data_full[ii][2,5,8,11,14,17]
-        y_vals_temp = data_full[ii][3,6,9,12,15,18]
-        z_vals_temp = data_full[ii][4,7,10,13,16,19]
+        data_3d = np.zeros((num_markers,3))
 
-        distances =
+        # compile x,y,z values
+        data_3d[:,0] = data_full[ii][[2,5,8,11,14,17]]
+        data_3d[:,1] = data_full[ii][[3,6,9,12,15,18]]
+        data_3d[:,2] = data_full[ii][[4,7,10,13,16,19]]
+
 
         # max distance between pts
+        max_dist = -1.0
+        for pt_combo in list(combinations(data_3d, 2)):
+            print(pt_combo)
+            dist = np.linalg.norm(pt_combo[1]-pt_combo[0])
+            if dist>max_dist:
+                max_dist = dist
 
         # min distance between pts
+        min_dist = np.inf
+        for pt_combo in list(combinations(data_3d, 2)):
+            print(pt_combo)
+            dist = np.linalg.norm(pt_combo[1] - pt_combo[0])
+            if dist < min_dist:
+                min_dist = dist
 
         # average distance between pts
+        total_dist = 0.0
+        for pt_combo in list(combinations(data_3d, 2)):
+            total_dist = total_dist + np.linalg.norm(pt_combo[1] - pt_combo[0])
+        average_dist = total_dist/len(list(combinations(data_3d, 2)))
 
-        # angles and stuff
+        # max angle
+
+
+        # min angle
+
+
+        # average angle
 
 
     # normalize the features
